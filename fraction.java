3eb1e472bcadc89d37d4ab3352ff0c2bdf718f55
bouzeggaoui manal <bouzeggaouimanal@gmail.com>
2025-09-23 21:35:54 +0200

    ajouter dans la Classe Fraction

--- fraction.java
+++ fraction.java
@@ -4,6 +4,21 @@ public class fraction {
      public String toString() {
         return "Je suis une fraction.";
     }
+
+    public Fraction(int numerateur, int denominateur) {
+    if (denominateur == 0) {
+        throw new IllegalArgumentException("Le dénominateur ne peut pas être nul.");
+    }
+    this.numerateur = numerateur;
+    this.denominateur = denominateur;
+}
+public int getNumerateur() {
+    return numerateur;
+}
+
+public int getDenominateur() {
+    return denominateur;
+}
     }
 
     
